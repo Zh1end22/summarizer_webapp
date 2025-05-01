128be390947d762f781cfb221bc6a5380e0f2850
Antriksh Dubey <121251852+Zh1end22@users.noreply.github.com>
2025-05-02 01:16:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import subprocess
+import os
 
 app = Flask(__name__)
 
@@ -27,4 +28,5 @@ def summarize():
     return render_template('index.html', summary=summary_output)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
